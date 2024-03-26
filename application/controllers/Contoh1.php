401e3ab77023ec2a065eb688344f071799480e5d
Reza <rezamutinur@gmail.com>
2024-03-26 20:14:16 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Reza
+            Saya Tinggal di Depok
+            Hobi Saya Bermain Layang-Layang";   
+    }
+}
